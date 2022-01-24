c44546c6151fc5597a2b6cf68f22b827cad18a87
Afifor <84326041+Afifor@users.noreply.github.com>
2022-01-24 17:25:36 +0100

    loaduser method

---
+++ src/main/java/com/wotflix/service/UserService.java
@@ -0,0 +1,25 @@
+package com.wotflix.service;
+
+import com.wotflix.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class UserService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    private final static String USER_NOT_FOUND_MSG = "user with email %s not found";
+
+
+    //Method used by spring security to login a user
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException(String.format(USER_NOT_FOUND_MSG, email)));
+    }
+}
